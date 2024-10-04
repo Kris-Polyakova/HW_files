ae853dc8fac88b014790ecb262ea5c2efcfabdd5
Kristina_Polyakova_FPU-115 <kris1617polyakova@yandex.ru>
2024-10-04 20:42:10 +0700

    exercise №2

--- work_with_file.py
+++ work_with_file.py
@@ -16,7 +16,25 @@
             ingredients['quantity'] = int(splitted_line[1])
             ingredients['measure'] = splitted_line[2]
             cook_book[dish].append(ingredients)
+            
+dishes = ['Омлет', 'Омлет с беконом', 'Запеченный картофель', 'Десерт с вишней']
 
-
-
-
+def get_buy_list_by_dishes(dishes = list, person_count = int):
+    shopping_dict = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for product in cook_book[dish]:
+                product_key = product['ingredient_name']
+                product_value = {}
+                if product_key in shopping_dict:
+                    old_value = shopping_dict[product_key].get('quantity')
+                    new_value = product.get('quantity') * person_count
+                    product_value['quantity'] = int(old_value + new_value)
+                else:
+                    shopping_dict[product_key] = {}
+                    product_value['measure'] = product.get('measure')
+                    product_value['quantity'] = product.get('quantity') * person_count
+                shopping_dict[product_key].update(product_value)
+    return shopping_dict
+   
+print(get_buy_list_by_dishes(dishes,1))
